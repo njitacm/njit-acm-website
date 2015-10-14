fe95ea23c4675cad63fc242fb87c482a0a673cb1
Binoy Patel <binoypatel14@gmail.com>
2015-10-14 13:58:44 -0400

    Adds calendar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -70,4 +70,17 @@
   }
 
   // Your custom JavaScript goes here
+
+	$('#calendar').fullCalendar({
+		googleCalendarApiKey: 'AIzaSyDMz_FvSnQ4_Lg-tamBTc_IPwIyyeKP5vE',
+    events: {
+        googleCalendarId: '9niid0lakg17i3rblct46bcafo@group.calendar.google.com'
+    },
+		defaultView : 'agendaWeek',
+		minTime : '08:00:00',
+		maxTime : '22:00:00',
+		height : 700,
+		allDaySlot : false,
+		hiddenDays : [0,6]
+	});
 })();
