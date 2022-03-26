6d2f4fce37f735e3bce1b4ffeb1c9c719ab5949b
Alex <adruzenko03@gmail.com>
2022-03-26 18:31:11 -0400

    Add Caroseul+Revert to Vue2 + Bootstrap packages

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import { BootstrapVue} from 'bootstrap-vue'
+import Vue from 'vue'
 
-createApp(App).mount('#app')
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
