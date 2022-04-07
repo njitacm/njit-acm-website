a8b0f55a01880f4cc53d0680811dc62d013d402e
WokFriedE <47959663+WokFriedE@users.noreply.github.com>
2022-04-07 00:51:56 -0400

    Added basic info, redesigned the icons, added spaces for logos

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,21 +3,58 @@
     <div class="footer">
       <b-container class="footer-boot">
         <b-row>
-          <b-col class="links">
-            <p>hi1</p>
+          <b-col name="Logos">
+            <img
+              src="https://www.gstatic.com/images/branding/product/2x/keep_48dp.png"
+              name="NJIT"
+            />
+            <img
+              src="https://www.gstatic.com/images/branding/product/2x/keep_48dp.png"
+              name="ACM"
+            />
+            <img
+              src="https://www.gstatic.com/images/branding/product/2x/keep_48dp.png"
+              name="YWCC"
+            />
           </b-col>
-          <b-col cols="8" name="foot-middle">
+          <b-col class="links">
+            <h5>Links</h5>
+            <div name="link-redirects">
+              <a href="#">Constitution</a>
+            </div>
+          </b-col>
+          <b-col class="contact-us" cols="5">
+            <h5>Contact Us</h5>
             <b-row>
-              <ul id="social-cards" name="social-icons">
-                <li v-for="value in socials" v-bind:key="value.title">
-                  <a v-bind:href="value.url">{{ value.title }}</a>
-                </li>
-              </ul>
+              <b-col>
+                <ul class="contact-info">
+                  <li style="font-size: 105%">Phone:</li>
+                  <li style="font-size: 90%">222-222-2222</li>
+                  <li style="font-size: 105%">Email:</li>
+                  <li style="font-size: 90%">acm.njit.edu</li>
+                </ul>
+              </b-col>
+              <b-col>
+                <ul class="contact-info">
+                  <li style="font-size: 105%">Located:</li>
+                  <li style="font-size: 90%">GITC 3704</li>
+                  <li style="font-size: 90%">
+                    218 Central Ave, Newark, NJ 07102
+                  </li>
+                </ul>
+              </b-col>
             </b-row>
           </b-col>
-          <b-col class="contact-us">
-            <p>hi4</p>
-          </b-col>
+        </b-row>
+
+        <b-row class="text-center">
+          <b-nav v-for="value in socials" v-bind:key="value.title">
+            <b-col>
+              <b-nav-item v-bind:href="value.url">
+                <img v-bind:src="value.img_source" />
+              </b-nav-item>
+            </b-col>
+          </b-nav>
         </b-row>
       </b-container>
     </div>
@@ -31,47 +68,56 @@ export default {
       socials: [
         {
           title: "YouTube",
-          img_source: "",
+          img_source:
+            "https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/youtube-48.png",
           url: "https://www.youtube.com/channel/UC3stSn7UK5-IX6mNnlWlRLw",
         },
         {
           title: "Twitter",
-          img_source: "",
+          img_source:
+            "https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/twitter-48.png",
           url: "https://twitter.com/NJITACM",
         },
         {
           title: "LinkedIn",
-          img_source: "",
+          img_source:
+            "https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/youtube-48.png",
           url: "",
         },
         {
           title: "Discord",
-          img_source: "",
+          img_source:
+            "https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/youtube-48.png",
           url: "https://njit.acm.org/discord",
         },
         {
           title: "Instagram",
-          img_source: "",
+          img_source:
+            "https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/youtube-48.png",
           url: "https://www.instagram.com/NJITACM/",
         },
         {
           title: "Facebook",
-          img_source: "",
+          img_source:
+            "https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/youtube-48.png",
           url: "https://www.facebook.com/groups/njtacm",
         },
         {
           title: "Twitch",
-          img_source: "",
+          img_source:
+            "https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/youtube-48.png",
           url: "https://www.twitch.tv/NJITACM",
         },
         {
           title: "TikTok",
-          img_source: "",
+          img_source:
+            "https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/youtube-48.png",
           url: "",
         },
         {
           title: "Reddit",
-          img_source: "",
+          img_source:
+            "https://cdn3.iconfinder.com/data/icons/popular-services-brands/512/youtube-48.png",
           url: "",
         },
       ],
@@ -87,38 +133,39 @@ footer {
   text-align: center;
   position: relative;
   bottom: 0%;
-  margin-bottom: -16px;
-}
-
-ul {
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  list-style-type: none;
-  background-color: darkred;
-  justify-content: center;
-  text-align: center;
-}
-
-li {
-  float: left;
-}
-
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 16px;
-  text-decoration: none;
-  justify-content: center;
+  margin-bottom: -11px;
+  padding-top: 5px;
 }
 
 li a:hover {
-  background-color: #111111;
+  background-color: #5f2121;
 }
 
-.foot-middle {
-  justify-content: center;
-  text-align: center;
+a:hover {
+  border-radius: 6px;
+  background-color: #5f2121;
+}
+
+.contact-us {
+  border: 1px solid #5f2121;
+  margin-left: 5px;
+  margin-right: 5px;
+}
+
+.links {
+  border: 1px solid #5f2121;
+  margin-left: 5px;
+  margin-right: 5px;
+}
+
+.contact-info {
+  float: initial;
+  overflow: auto;
+  margin-top: -10px;
+  list-style: none;
+  text-align: left;
+}
+
+.link-redirects {
 }
 </style>
