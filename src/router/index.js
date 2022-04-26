49d487e4bb5e5a8cfbcaff96bd7da2593e658e3c
Alex <adruzenko03@gmail.com>
2022-04-26 10:24:57 -0400

    Revert "tutoring changes"
    This reverts commit 017d93feedcf959542180f6157147c7af414cfda.

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import VueRouter from "vue-router";
 import Home from "../views/Home";
 import Sigs from "../views/Sigs";
 import Events from "../views/Events";
-import Tutoring from "../views/Tutoring";
 
 Vue.use(VueRouter);
 
@@ -17,11 +16,6 @@ const routes = [
     path: "/sigs",
     name: "SIGS",
     component: Sigs
-  },
-  {
-    path: "/Tutoring",
-    name: "Tutoring",
-    component: Tutoring
   },
   {
     path: "/events",
