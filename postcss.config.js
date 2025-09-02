76b3c47ea45d0fad574b7cae7dfbc860ba13f0b9
Krish A. Patel <krishpatel2067@gmail.com>
2025-09-02 15:16:11 -0400

    Update config

--- postcss.config.js
+++ postcss.config.js
@@ -3,26 +3,28 @@ const IN_PRODUCTION = process.env.NODE_ENV === "production";
 module.exports = {
   plugins: [
     IN_PRODUCTION &&
-      require("@fullhuman/postcss-purgecss")({
-        content: [`./public/**/*.html`, `./src/**/*.vue`],
-        defaultExtractor(content) {
-          const contentWithoutStyleBlocks = content.replace(
-            /<style[^]+?<\/style>/gi,
-            ""
-          );
-          return (
-            contentWithoutStyleBlocks.match(
-              /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
-            ) || []
-          );
-        },
-        safelist: [
-          /-(leave|enter|appear)(|-(to|from|active))$/,
-          /^(?!(|.*?:)cursor-move).+-move$/,
-          /^router-link(|-exact)-active$/,
-          /data-v-.*/,
-          "data-theme",     // to ensure light / dark theme functionality works
-        ],
-      }),
+    require("@fullhuman/postcss-purgecss")({
+      content: [`./public/**/*.html`, `./src/**/*.vue`],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(
+          /<style[^]+?<\/style>/gi,
+          ""
+        );
+        return (
+          contentWithoutStyleBlocks.match(
+            /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
+          ) || []
+        );
+      },
+      safelist: {
+        pattern: [
+          // /-(leave|enter|appear)(|-(to|from|active))$/,
+          // /^(?!(|.*?:)cursor-move).+-move$/,
+          // /^router-link(|-exact)-active$/,
+          // /data-v-.*/,
+          /data-theme/,     // to ensure light / dark theme functionality works
+        ]
+      },
+    }),
   ],
 };
