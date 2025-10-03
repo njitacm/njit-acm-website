8357d594d3c30c7d2e8c7844063a5b404af390d6
Krish A. Patel <krishpatel2067@gmail.com>
2025-10-02 20:37:42 -0400

    Finish GitHub event form component

--- src/components/GitHubEventForm.vue
+++ src/components/GitHubEventForm.vue
@@ -1,6 +1,25 @@
 <template>
-
+    <div class="GitHubEventForm">
+        <h1>[GitHub Event Name]</h1>
+        <h2>[Date, Location]</h2>
+        <p>[Description]</p>
+        <PrimaryButton>
+            <a href="[google form link here]">Sign Up</a>
+        </PrimaryButton>
+    </div>
 </template>
 
+<script>
+import PrimaryButton from './PrimaryButton.vue';
+
+export default {
+    components: { PrimaryButton }
+}
+</script>
+
 <style scoped>
+.GitHubEventForm {
+    display: flex;
+    gap: 4px;
+}
 </style>
